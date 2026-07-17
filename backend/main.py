78f543d8a7682d5ecadfc99837bd6a2650033d46
Francis Shaba Miyoba <fomocopixel@gmail.com>
2026-07-17 13:04:17 -0500

    testing coderabbit integration

--- backend/main.py
+++ backend/main.py
@@ -2,19 +2,24 @@ from fastapi import FastAPI
 from routers.health import router as health_router
 from routers.info import router as info_router
 from routers.status import router as status_router
+from routers.welcome import router as welcome_router
+from config import APP_NAME, APP_VERSION
+from routers.projects import router as projects_router
 
 app = FastAPI(
-    title="CloudSage API",
-    version="0.1.0"
+    title=APP_NAME,
+    version=APP_VERSION
 )
 
 app.include_router(health_router)
 app.include_router(info_router)
 app.include_router(status_router)
+app.include_router(welcome_router)
+app.include_router(projects_router)
 
 @app.get("/")
 def root():
     return {
         "status": "CloudSage Backend Running",
-        "version": "0.1.0"
+        "version": APP_VERSION
     }
